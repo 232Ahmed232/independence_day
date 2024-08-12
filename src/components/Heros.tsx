885f77735bb3f0a48cdd3f3503f43cbf89f48789
232Ahmed232 <x99aliahmed@gmail.com>
2024-08-12 10:38:58 +0500

    again

--- src/components/Heros.tsx
+++ src/components/Heros.tsx
@@ -38,6 +38,7 @@ function Heros() {
           return (
             
               <PinContainer
+              key={ele.achieve}
               >
                 <div className="flex  basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] bg-gradient-to-r from-green-500 to-green-800 ">
                   <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
