9735cbebd2f9a493d9effbdc7e5315c2fdc43b04
232Ahmed232 <x99aliahmed@gmail.com>
2024-08-12 16:02:50 +0500

    change into passion

--- src/components/Heros.tsx
+++ src/components/Heros.tsx
@@ -6,22 +6,26 @@ import Image from 'next/image'
 
 const heroes = [
   {
-    name: "Imran Khan",
-    achieve: "World Cup winner in 1992 Cricket",
-    pic: "https://res.cloudinary.com/dpviaxj2y/image/upload/v1723438205/58188af0bcec7_w866co.jpg"
+    name: "Fatima Jinnah",
+    description:`Known as the "Mother of the Nation," Fatima Jinnah was a close adviser and sister to Muhammad Ali Jinnah. She played an essential role in the Pakistan Movement and later became a symbol of democracy and women's rights.`,
+    achieve: "Politician, Dental Surgeon",
+    pic: "https://res.cloudinary.com/dpviaxj2y/image/upload/v1723460422/Fatima-Jinnah_okztit.jpg"
   },
   {
-    name: "Shabaz Ahmed",
-    achieve: "World Cup winner in 1994 Hockey",
-    pic: "https://res.cloudinary.com/dpviaxj2y/image/upload/v1723438309/42174-shahbaz-ahmed-senior-pakistan-hockey_lo3wzs.webp"
+    name: "Dr. Abdul Qadeer Khan",
+    description:"Known as the father of Pakistan's nuclear program, Dr. Abdul Qadeer Khan played a crucial role in making Pakistan the first Islamic nuclear power by leading the country's atomic program.",
+    achieve: "Nuclear Scientist",
+    pic: "https://res.cloudinary.com/dpviaxj2y/image/upload/v1723460423/20811719271696907418-1280x720_kvuvs8.webp"
   },
   {
     name: "Muhammad Ali Jinnah",
+    description:" Jinnah, also known as Quaid-e-Azam (Great Leader), was the leader of the All India Muslim League and played a pivotal role in the creation of Pakistan in 1947 as a separate nation for Muslims of the Indian subcontinent.",
     achieve: "Founder of Pakistan",
     pic: "https://res.cloudinary.com/dpviaxj2y/image/upload/v1723440611/qaid-e1703252891153_nkq67i.jpg"
   },
   {
     name: "Allama Muhammad Iqbal ",
+    description:"  Iqbal is regarded as the ideological father of Pakistan. His poetry and philosophy inspired the Muslim community in India to seek an independent state, and he was one of the first to propose the idea of a separate Muslim nation.",
     achieve: "Poet, Philosopher, Politician",
     pic: "https://res.cloudinary.com/dpviaxj2y/image/upload/v1723440710/5fa90ac64d24f_ribabx.jpg"
   },
@@ -39,6 +43,7 @@ function Heros() {
             
               <PinContainer
               key={ele.achieve}
+              title={ele.description}
               >
                 <div className="flex  basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] bg-gradient-to-r from-green-500 to-green-800 ">
                   <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
